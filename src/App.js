5561b4ecfdedcd570aa9b15fc3431c31f4f644b6
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-01-02 16:33:29 +0300

    react radio app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import { DATA } from './Data';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className='header'>Radio</div>
+        <div className = "App">
+      <div className='radio-station card-shadow'>
+      <div className='logo-back'>
+        <img src="https://www.radyo45lik.com/images/radyo-45lik-logo1aa-768x4881-1-600x600.png"/>
+        </div>
+        <a className='play-icon'><i className='icons ion-ios-play'></i></a>
+      </div>
+      <div className='radio-station card-shadow'>
+        <div className='logo-back'>
+        <img src="https://www.slowturk.com.tr/assets/img/logo-slowturk.png"/>
+        </div>
+        <a className='play-icon'><i className='icons ion-ios-play'></i></a>
+      </div>
+      <div className='radio-station card-shadow'>
+        <div className='logo-back'>
+        <img src="https://static.wikia.nocookie.net/logopedia/images/3/36/Alem_fm_logo_2.png"/>
+         </div>
+         <a className='play-icon'><i className='icons ion-ios-play'></i></a>
+      </div>
+      <div className='radio-station card-shadow'>
+      <div className='logo-back'>
+        <img src="https://upload.wikimedia.org/wikipedia/tr/6/61/JoyTurk_logosu.png"/>
+        </div>
+        <a className='play-icon'><i className='icons ion-ios-play'></i></a>
+      </div>
+      <div className='radio-station card-shadow'>
+       <div className='logo-back'>
+        <img src="https://www.numberone.com.tr/wp-content/uploads/2015/12/N1_FM-1.png"/>
+      </div>
+        <a className='play-icon'><i className='icons ion-ios-play'></i></a>
+      </div>
+      <div className='radio-station card-shadow'>
+       <div className='logo-back'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/TRT_FM_logo.svg/1200px-TRT_FM_logo.svg.png"/>
+      </div>
+        <a className='play-icon'><i className='icons ion-ios-play'></i></a>
+      </div>
+      <div className='radio-station card-shadow'>
+       <div className='logo-back'>
+        <img src="https://www.onlineradyodinle.com.tr/kanal/super-fm.png"/>
+      </div>
+        <a className='play-icon'><i className='icons ion-ios-play'></i></a>
+      </div>
+      <div className='radio-station card-shadow'>
+       <div className='logo-back'>
+        <img src="https://www.canliradyodinle.fm/wp-content/uploads/pal-nostalji-dinle.png"/>
+      </div>
+        <a className='play-icon'><i className='icons ion-ios-play'></i></a>
+      </div>
+      <div className='radio-station card-shadow'>
+       <div className='logo-back'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Best_FM.png/230px-Best_FM.png"/>
+      </div>
+        <a className='play-icon'><i className='icons ion-ios-play'></i></a>
+      </div>
+      <div className='radio-station card-shadow'>
+       <div className='logo-back'>
+        <img src="https://upload.wikimedia.org/wikipedia/en/d/d2/Metro_FM_%282013_Logo%29.gif"/>
+      </div>
+        <a className='play-icon'><i className='icons ion-ios-play'></i></a>
+      </div>
+      
     </div>
-  );
+    </div>
+    
+    
+  ) 
 }
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,14 @@
+export const DATA = [
+    {id:1,url:'http://stream.radyo45lik.com:4545/stream',image:'https://www.radyo45lik.com/images/radyo-45lik-logo1aa-768x4881-1-600x600.png'},
+    {id:2,url:'https://r3.rocketcdn.com/slowturk/abr/playlist.m3u8',image:'https://www.slowturk.com.tr/assets/img/logo-slowturk.png'},
+    {id:3,url:'http://turkmedya.radyotvonline.com/turkmedya/alemfm.stream/playlist.m3u8',image:'https://static.wikia.nocookie.net/logopedia/images/3/36/Alem_fm_logo_2.png/revision/latest?cb=20210804081906'},
+    {id:4,url:'http://provisioning.streamtheworld.com/pls/JOY_TURKAAC.pls',image:'https://upload.wikimedia.org/wikipedia/tr/6/61/JoyTurk_logosu.png'},
+    {id:5,url:'https://muzikliste.blogspot.com/2017/01/number-one-fm.html',image:'https://www.numberone.com.tr/wp-content/uploads/2015/12/N1_FM-1.png'},
+    {id:6,url:'http://trtcanlifm-lh.akamaihd.net/i/TRTFM_1@181846/master.m3u8',image:'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/TRT_FM_logo.svg/1200px-TRT_FM_logo.svg.png'},
+    {id:7,url:'http://provisioning.streamtheworld.com/pls/SUPER_FMAAC.pls',image:'https://www.onlineradyodinle.com.tr/kanal/super-fm.png'},
+    {id:8,url:'http://shoutcast.radyogrup.com:1010/;',image:'https://www.canliradyodinle.fm/wp-content/uploads/pal-nostalji-dinle.png'},
+    {id:9,url:'http://46.20.7.125/listen.pls',image:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Best_FM.png/230px-Best_FM.png'},
+    {id:10,url:'http://provisioning.streamtheworld.com/pls/METRO_FMAAC.pls',image:'https://upload.wikimedia.org/wikipedia/en/d/d2/Metro_FM_%282013_Logo%29.gif'},
+
+
+];
