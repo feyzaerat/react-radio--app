f3f9cc789eb89c1f1ce85174dabbcd16cf414086
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-01-02 18:33:50 +0300

    player css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import { DATA } from './Data';
+import  Audioplayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 import './App.css';
 
 
 
 function App() {
+
+const [url, setUrl] = useState(null)
+const changeUrl = (url) => {
+  setUrl(url);
+}
+
   return (
     <div>
      <div className='header'>Radio</div>
@@ -14,12 +22,22 @@ function App() {
       <div className='logo-back'>
         <img src={item.image}/>
         </div>
-        <a className='play-icon'><i className='icons ion-ios-play'></i></a>
+        <a onClick={() =>setUrl(item.url)} className='play-icon'><i className='icons ion-ios-play'></i></a>
       </div>
       )})}
+      
+     </div>
+     <div>
+     <Audioplayer
+     showJumpControls={false}
+    src={url}
+    onPlay={e => console.log("onPlay")}
+    // other props here
+  />
      </div>
     </div>
   ) 
+  
 }
 
 export default App;
