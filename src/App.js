5813cf3d91ea209f9d1f303d4294221ff9c0824c
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-01-31 17:36:49 +0300

    created copyright text

--- src/App.js
+++ src/App.js
@@ -1,48 +1,59 @@
-import React, {useState, useEffect} from 'react';
-import { DATA } from './Data';
-import  Audioplayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
-import './App.css';
-
-
+import React, { useState, useEffect } from "react";
+import { DATA } from "./Data";
+import Audioplayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import "./App.css";
 
 function App() {
- 
-const [url, setUrl] = useState(null)
-const changeUrl = (url) => {
-  setUrl(url);
-}
-
+  const [url, setUrl] = useState(null);
+  const changeUrl = (url) => {
+    setUrl(url);
+  };
+  const Year = new Date().getFullYear();
 
   return (
     <div>
-     <div className='header'><a className='web-link' href='https://feyzaerat.com'>FKE</a> Radio</div>
-     <div className = "App">
-      {DATA.map((item) => {return (
-        <div className='radio-station card-shadow'>
-      <div className='logo-back'>
-        <img src={item.image}/>
-        </div>
-        <a onClick={() =>setUrl(item.url)} className='play-icon'
-        
-        ><i className='icons ion-ios-play'></i></a>
+      <div className="header">
+        <a className="web-link" href="https://feyzaerat.com">
+          FKE
+        </a>{" "}
+        Radio
       </div>
-      )})}
-      
-     </div>
-  
-     <div>
-     <Audioplayer
-    onPlay={e => console.log("onPlay")}
-    showJumpControls={false}
-    src={url}
-    
-   
-  />
-     </div>
+      <div className="App">
+        {DATA.map((item) => {
+          return (
+            <div className="radio-station card-shadow">
+              <div className="logo-back">
+                <img src={item.image} />
+              </div>
+              <a onClick={() => setUrl(item.url)} className="play-icon">
+                <i className="icons ion-ios-play"></i>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+
+      <div>
+        <Audioplayer
+          onPlay={(e) => console.log("onPlay")}
+          showJumpControls={false}
+          src={url}
+        />
+        <div className="footer">
+          <a>
+            Copyright &copy; {Year}{" "}
+            <span>
+              <a className="web-target" href="https://feyzaerat.com">
+                FKE
+              </a>
+            </span>
+            , All rights reserved
+          </a>
+        </div>
+      </div>
     </div>
-  ) 
-  
+  );
 }
 
 export default App;
